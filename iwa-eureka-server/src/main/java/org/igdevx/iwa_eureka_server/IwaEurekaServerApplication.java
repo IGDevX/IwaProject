035563e26ecc9e87b052f6bad1d1b10de48104b6
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-10-04 17:48:44 +0200

    chore: added eureka server

---
+++ iwa-eureka-server/src/main/java/org/igdevx/iwa_eureka_server/IwaEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package org.igdevx.iwa_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwaEurekaServerApplication.class, args);
+	}
+
+}
---
+++ iwa-eureka-server/src/test/java/org/igdevx/iwa_eureka_server/IwaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igdevx.iwa_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
