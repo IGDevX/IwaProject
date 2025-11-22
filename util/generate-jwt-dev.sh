f734faa28113519966f8aa92002df6e1b86b4211
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-11-22 17:09:29 +0100

    feat(dev): added kafka to dev env

--- util/generate-jwt-dev.sh
+++ util/generate-jwt-dev.sh
@@ -4,10 +4,5 @@ curl -s -X POST "http://localhost:3000/realms/marche-conclu/protocol/openid-conn
   -d "grant_type=password" \
   -d "username=test" \
   -d "password=test" \
-| jq -r '
-  "access_token:", .access_token, "",
-  "refresh_token:", .refresh_token, "",
-  "expires_in:", (.expires_in|tostring), "",
-  "refresh_expires_in:", (.refresh_expires_in|tostring), "",
-  "token_type:", .token_type
-'
+| jq -r '.access_token' | tee .jwt
+
